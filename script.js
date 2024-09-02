df918a918ee87a981ab2148fd8fe368355dd46a4
Hojimurod <kamolovhojimurod08@gmail.com>
2024-09-02 11:03:58 +0500

    create html

---
+++ script.js
@@ -0,0 +1,15 @@
+var name = prompt("Ismingizni kiriting");
+var age = prompt("Yoshingizni kiriting");
+var misol = prompt("10+20=?");
+var ayirish = prompt("40-20=?");
+var kopaytirish = prompt("20*10=?");
+var bolish = prompt("72/8=?");
+
+console.log("Sizning ismingiz: " + name);
+console.log("Sizning yoshingiz: " + age);
+console.log("10+20=30 (sizning javobingiz: " + misol + ")");
+console.log("40-20=20 (sizning javobingiz: " + ayirish + ")");
+console.log("20*10=200 (sizning javobingiz: " + kopaytirish + ")");
+console.log("72/8=9 (sizning javobingiz: " + bolish + ")");
+
+alert("Natijalarni ko'rish uchun konsolni oching!");
